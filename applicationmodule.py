2e0218a515c97532ddd22b9705d20cd6e97e3266
Mario Baranzini <mario@opengis.ch>
2017-10-03 08:18:06 +0200

    clean applicationmodule.py

--- applicationmodule.py
+++ applicationmodule.py
@@ -12,25 +12,3 @@ class ApplicationModule(ApplicationModuleBase):
     """
     def __init__(self, veriso):
         super(ApplicationModule, self).__init__(veriso)
-
-        # TODO connect to a signal sent when a new layer is loaded to call
-        # translations
-
-        self.iface = veriso.iface
-
-        #self.legend = self.iface.legendInterface()
-        #self.legend.itemAdded.connect(self.translate)
-
-    def translate(self, i):
-
-        from veriso.opengisch_utils.qgis.layers_translator.layers_translator \
-            import QgisLayersTranslator, Dictionary
-
-        current_dir = os.path.dirname(os.path.abspath(__file__))
-        dictionary_file = os.path.join(current_dir, 'translations_verivd.yml')
-
-        verivd_dictionary = Dictionary(dictionary_file)
-
-        layers_translator = QgisLayersTranslator(
-            self.iface, verivd_dictionary)
-        layers_translator.run()
